6730a0a9de351a8b571f5ed03c26b5b8fd4c87b7
sugjo <iluxa-com2001@yandex.ru>
2022-07-17 08:05:51 +0500

    add mdsvex

---
+++ mdsvex.config.js
@@ -0,0 +1,8 @@
+const config = {
+    extensions: [".svelte.md", ".md", ".svx"],
+
+    remarkPlugins: [],
+    rehypePlugins: [],
+};
+
+export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import mdsvexConfig from "./mdsvex.config.js";
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: [".svelte", ...mdsvexConfig.extensions],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+		mdsvex(mdsvexConfig),
+	],
 
 	kit: {
 		adapter: adapter()
